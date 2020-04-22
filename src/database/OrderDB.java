e7d0868a38d0ef4a36494bef2e42c13fb9ea56e6
tiagoprox <tiagoportugal46@gmail.com>
2020-04-22 19:02:40 +0200

    Implementation of interface

--- src/database/OrderDB.java
+++ src/database/OrderDB.java
@@ -1,5 +1,37 @@
 package database;
 
-public class OrderDB {
+import java.sql.ResultSet;
+
+public class OrderDB implements DBInterface<Order> {
+
+	@Override
+	public int create(Order value) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Order selectByID(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResultSet selectByString(String column, String value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(Order value) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Order value) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
