042e11a2e643823bc19d24efd80a3e39c8116210
Krisztián Henrik Papp <h3nk4s@henky.me>
2020-04-22 12:24:21 +0200

    Create the DBInterface

---
+++ src/database/DBInterface.java
@@ -0,0 +1,42 @@
+package database;
+
+import java.sql.ResultSet;
+
+/**
+ * This interface is used in connection with the DAO pattern
+ * @param <T> with this parameter you can set a type which you will use in later methods as parameters
+ */
+public interface DBInterface<T> {
+
+    /**
+     * This method takes an object and converts it to a valid SQL INSERT query, which is the executed
+     * @param value it's the given T type object
+     * @return the generated key after the insertion to the DB
+     * @see DBConnection executeInsertWithID() method
+     */
+    public int create(T value);
+
+    /**
+     * This method takes an object and converts it to a valid SQL SELECT query, which is the executed
+     * @param value it's the given T type object
+     * @return the ResultSet containing the result of the query
+     * @see DBConnection executeSelect() method
+     */
+    public ResultSet select(T value);
+
+    /**
+     * This method takes an object and converts it to a valid SQL UPDATE query, which is the executed
+     * @param value it's the given T type object
+     * @return the number of rows affected by the update
+     * @see DBConnection executeQuery() method
+     */
+    public int update(T value);
+
+    /**
+     * This method takes an object and converts it to a valid SQL DELETE query, which is the executed
+     * @param value it's the given T type object
+     * @return the number of rows deleted from the table
+     * @see DBConnection executeQuery()
+     */
+    public int delete(T value);
+}
